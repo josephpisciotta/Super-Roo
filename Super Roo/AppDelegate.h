71a72fa8fbe2f316aaba50399b7ff695ff96f256
Joseph Pisciotta <josephpisciotta@Josephs-MacBook-Air.local>
2013-03-03 20:43:32 -0500

    Initial Commit

---
+++ Super Roo/AppDelegate.h
@@ -0,0 +1,24 @@
+//
+//  AppDelegate.h
+//  Super Roo
+//
+//  Created by Joseph Pisciotta on 3/3/13.
+//  Copyright __MyCompanyName__ 2013. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "cocos2d.h"
+
+@interface AppController : NSObject <UIApplicationDelegate, CCDirectorDelegate>
+{
+	UIWindow *window_;
+	UINavigationController *navController_;
+
+	CCDirectorIOS	*director_;							// weak ref
+}
+
+@property (nonatomic, retain) UIWindow *window;
+@property (readonly) UINavigationController *navController;
+@property (readonly) CCDirectorIOS *director;
+
+@end
